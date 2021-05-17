3e4dee4117fab7de63369f8c7fabf34cd258c6dc
Andrey Yarovenko <stuffingeu56@gmail.com>
2021-05-17 18:04:20 +0300

    add enum

--- java_gb_2015/Java_Start_book_lesson/src/basic2/java0/section1/Size.java
+++ java_gb_2015/Java_Start_book_lesson/src/basic2/java0/section1/Size.java
@@ -1,5 +1,15 @@
 package basic2.java0.section1;
 
 public enum Size {
-    VERY_SMALL,SMALL, AVERAGE, BIG, VERY_BIG
+    VERY_SMALL("XS"),SMALL("S"), AVERAGE("M"), BIG("L"), VERY_BIG("XL");
+
+    private String abbreviation;
+
+    Size(String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
+
+    public String getAbbreviation() {
+        return abbreviation;
+    }
 }
