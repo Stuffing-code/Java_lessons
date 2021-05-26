130b0edee5eb978bac22aa6bcbcea53c04edbd77
Andrey Yarovenko <stuffingeu56@gmail.com>
2021-05-26 18:51:37 +0300

    exception

---
+++ java_gb_2015/Java_Start_book_lesson/src/basic2/java0/execeptions/ExceptionHandingMain.java
@@ -0,0 +1,11 @@
+package basic2.java0.execeptions;
+
+public class ExceptionHandingMain {
+    public static void main(String[] args) {
+        System.out.println(divide(10, 0));;
+    }
+
+    private static int divide(int numerator, int denumenator) {
+        return numerator / denumenator;
+    }
+}
