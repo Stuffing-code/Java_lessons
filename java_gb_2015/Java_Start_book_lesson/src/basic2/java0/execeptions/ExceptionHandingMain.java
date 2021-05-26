1340f888d1991b3e9a8afbaa176766a95b39cb28
Andrey Yarovenko <stuffingeu56@gmail.com>
2021-05-26 19:44:42 +0300

    exception

--- java_gb_2015/Java_Start_book_lesson/src/basic2/java0/execeptions/ExceptionHandingMain.java
+++ java_gb_2015/Java_Start_book_lesson/src/basic2/java0/execeptions/ExceptionHandingMain.java
@@ -1,11 +1,47 @@
 package basic2.java0.execeptions;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class ExceptionHandingMain {
+
     public static void main(String[] args) {
-        System.out.println(divide(10, 0));;
+        Scanner scanner = new Scanner(System.in);
+        boolean continueLoop = true;
+        PrintWriter writer = null;
+        do {
+            try {
+                System.out.println("Please enter numerator");
+                int numerator = scanner.nextInt();
+                System.out.println("Please enter denominator");
+                int denominator = scanner.nextInt();
+//                System.out.println(divide(numerator, denominator));
+//                safeToFail());
+                writer = new PrintWriter(new FileWriter("out.txt"));
+                writer.println("Result = " + divide(numerator, denominator));
+                continueLoop = false;
+            } catch (ArithmeticException | InputMismatchException e) {
+                System.out.println("Exception: " + e);
+                scanner.nextLine();
+                System.out.println("Only integer non-zero parameters allowed");
+            } catch (IOException e) {
+                System.out.println("Unable to open file");
+                e.printStackTrace();
+            } finally {
+                System.out.println("Finaly block called");
+                writer.close();
+            }
+        } while (continueLoop);
     }
 
-    private static int divide(int numerator, int denominator) {
+    private static int divide(int numerator, int denominator) throws ArithmeticException, NullPointerException {
         return numerator / denominator;
     }
+//
+//    private static void safeToFail(int result) throws IOException {
+//
+//    }
 }
