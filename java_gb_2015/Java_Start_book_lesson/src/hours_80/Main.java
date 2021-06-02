a4ad11a675bbbdc6dc958e54e3e25134bed6a5f8
Andrey Yarovenko <stuffingeu56@gmail.com>
2021-06-02 19:48:02 +0300

    refactoring, add methods

--- java_gb_2015/Java_Start_book_lesson/src/hours_80/Main.java
+++ java_gb_2015/Java_Start_book_lesson/src/hours_80/Main.java
@@ -1,16 +1,16 @@
 package hours_80;
 
-import static hours_80.Root_search.rootSearch;
+import static hours_80.Root_search.solveAnyEquitation;
 
 public class Main {
     public static void main(String[] args) {
 
-        rootSearch(1, 12, 36);
-        rootSearch(-1, -2, 15);
-        rootSearch(0, 0, 0);
-        rootSearch(1, -2, -3);
-        rootSearch(1, 0, 0);
-
+        solveAnyEquitation(1, 12, 36);
+        solveAnyEquitation(-1, -2, 15);
+        solveAnyEquitation(0, 0, 0);
+        solveAnyEquitation(1, -2, -3);
+        solveAnyEquitation(1, 0, 0);
+        solveAnyEquitation(0, 0, 1);
     }
 
 
--- java_gb_2015/Java_Start_book_lesson/src/hours_80/Root_search.java
+++ java_gb_2015/Java_Start_book_lesson/src/hours_80/Root_search.java
@@ -2,26 +2,37 @@ package hours_80;
 
 public class Root_search {
     //    (a*(x**2)) + (b*x) + c == 0
-    public static void rootSearch(int a, int b, int c) {
+    public static void solveAnyEquitation(int a, int b, int c) {
 
-        if (a == 0 && b != 0 && c != 0) {
-            double result = ((double) -c / b);
-            System.out.println(result);
-        } else if (a == 0 && b == 0 && c != 0) {
-            System.out.println("No rational solution, cannot be divisible by 0");
-        } else if (a == 0 && b == 0 && c == 0) {
+        if (a == 0 && b == 0 && c == 0) {
             System.out.println("result any number");
+        } else if (a != 0) {
+            solveRootEquitation(a, b, c);
         } else {
-            int discriminant = ((b * b) - (4 * a * c));
-            int result_1 = (int) ((-b + Math.sqrt(discriminant)) / (2 * a));
-            int result_2 = (int) ((-b - Math.sqrt(discriminant)) / (2 * a));
-            if (discriminant == 0) {
-                System.out.println("result = " + result_1);
-            } else if (discriminant > 0) {
-                System.out.println("result's = " + result_1 + ", " + result_2 + ".");
-            } else {
-                System.out.println("No rational solution");
-            }
+            solveLinearEquitation(b, c);
         }
     }
+
+    public static void solveRootEquitation(int a, int b, int c) {
+        int discriminant = ((b * b) - (4 * a * c));
+        int result_1 = (int) ((-b + Math.sqrt(discriminant)) / (2 * a));
+        int result_2 = (int) ((-b - Math.sqrt(discriminant)) / (2 * a));
+        if (discriminant == 0) {
+            System.out.println("result = " + result_1);
+        } else if (discriminant > 0) {
+            System.out.println("result's = " + result_1 + ", " + result_2 + ".");
+        } else {
+            System.out.println("No rational solution");
+        }
+    }
+
+    public static void solveLinearEquitation(int b, int c) {
+        if (b != 0) {
+            double result = ((double) -c / b);
+            System.out.println(result);
+        } else {
+            System.out.println("No rational solution, cannot be divisible by 0");
+        }
+    }
+
 }
