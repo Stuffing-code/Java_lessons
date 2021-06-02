42eb5c621d55926c2fbe1236c2539dd226bb4bf2
Andrey Yarovenko <stuffingeu56@gmail.com>
2021-06-02 19:23:04 +0300

    delete while

--- java_gb_2015/Java_Start_book_lesson/src/hours_80/Root_search.java
+++ java_gb_2015/Java_Start_book_lesson/src/hours_80/Root_search.java
@@ -4,29 +4,23 @@ public class Root_search {
     //    (a*(x**2)) + (b*x) + c == 0
     public static void rootSearch(int a, int b, int c) {
 
-        while (true) {
-            if (a == 0 && b != 0 && c != 0) {
-                double result = ((double)-c / b);
-                System.out.println(result);
-                break;
-            } else if (a == 0 && b == 0 && c != 0) {
-                System.out.println("No rational solution, cannot be divisible by 0");
-                break;
-            } else if (a == 0 && b == 0 && c == 0) {
-                System.out.println("result any number");
-                break;
+        if (a == 0 && b != 0 && c != 0) {
+            double result = ((double) -c / b);
+            System.out.println(result);
+        } else if (a == 0 && b == 0 && c != 0) {
+            System.out.println("No rational solution, cannot be divisible by 0");
+        } else if (a == 0 && b == 0 && c == 0) {
+            System.out.println("result any number");
+        } else {
+            int discriminant = ((b * b) - (4 * a * c));
+            int result_1 = (int) ((-b + Math.sqrt(discriminant)) / (2 * a));
+            int result_2 = (int) ((-b - Math.sqrt(discriminant)) / (2 * a));
+            if (discriminant == 0) {
+                System.out.println("result = " + result_1);
+            } else if (discriminant > 0) {
+                System.out.println("result's = " + result_1 + ", " + result_2 + ".");
             } else {
-                int discriminant = ((b * b) - (4 * a * c));
-                int result_1 = (int) ((-b + Math.sqrt(discriminant)) / (2 * a));
-                int result_2 = (int) ((-b - Math.sqrt(discriminant)) / (2 * a));
-                if (discriminant == 0) {
-                    System.out.println("result = " + result_1);
-                    break;
-                } else if (discriminant > 0) {
-                    System.out.println("result's = " + result_1 + ", " + result_2 + ".");
-                } else {
-                    System.out.println("No rational solution");
-                }
+                System.out.println("No rational solution");
             }
         }
     }
