aa4cbb8e55ffd7004f409579007ab775b4b0f794
Andrey Yarovenko <stuffingeu56@gmail.com>
2021-06-07 16:28:32 +0300

    Refactoring: add getters, setters, constructor, const and method.

--- java_gb_2015/Java_Start_book_lesson/src/hours_80/dz2/List_creator.java
+++ java_gb_2015/Java_Start_book_lesson/src/hours_80/dz2/List_creator.java
@@ -5,20 +5,46 @@ import java.util.LinkedList;
 import java.util.Random;
 
 public class List_creator {
+    int SIZE_BUFFER;
+    int INDEX_MAX;
     static int count = 0;
+
+    public List_creator(int SIZE_BUFFER) {
+        this.SIZE_BUFFER = SIZE_BUFFER;
+        this.INDEX_MAX = SIZE_BUFFER - 1;
+    }
+
+    public  int getSizeBuffer() {
+        return SIZE_BUFFER;
+    }
+
+    public int getIndexMax() {
+        return INDEX_MAX;
+    }
+
+    public void setSizeBuffer(int sizeBuffer) {
+        SIZE_BUFFER = sizeBuffer;
+        INDEX_MAX = sizeBuffer - 1;
+    }
 
     public static int randomNumber() {
         Random random = new Random();
         return random.nextInt(10);
     }
 
+    // if size buffer not even
+    public static int returnMedianArray(LinkedList<Integer> list) {
+        int index = (list.size() / 2);
+        return list.get(index);
+    }
+
     // solve median array
     public static double solveMedianDouble(int number_1, int number_2) {
-        return (double) (number_1 + number_2) /2;
+        return (double) (number_1 + number_2) / 2;
     }
 
     public static int solveMedianInt(int number_1, int number_2) {
-        return (number_1 + number_2) /2;
+        return (number_1 + number_2) / 2;
     }
 
     // check double or int type
@@ -27,11 +53,11 @@ public class List_creator {
     }
 
     // display array on the console
-    public static void displayList(LinkedList<Integer> list) {
+    public  void displayList(LinkedList<Integer> list) {
 //        System.out.print("List_creator: displayList(): [");
         System.out.print("[");
         for (int i = 0; i < list.size(); i++) {
-            if (i < 7) {
+            if (i < getIndexMax()) {
                 System.out.print(list.get(i) + ",");
             } else {
                 System.out.print(list.get(i));
@@ -40,33 +66,47 @@ public class List_creator {
         System.out.print("] -> ");
     }
 
-//    main method
-    public static void fifo(LinkedList<Integer> list, int number) {
-        if (list.size() == 8) {
-            LinkedList<Integer> list_temp = new LinkedList<>(list); // create temp arry
-            displayList(list);
-//            displayList(list_temp);
-//            System.out.println();
-            Collections.sort(list_temp); // sort list
-//            displayList(list);
-            displayList(list_temp);
+    // TODO: насколько это целесообразно?
+    public void printUnsortedPart(LinkedList<Integer> list) {
+        displayList(list);
+    }
+
+    public void printSortedPart(LinkedList<Integer> list_temp) {
+        Collections.sort(list_temp); // sort list
+        displayList(list_temp);
+
+    }
+
+    public  void printMedianValue(LinkedList<Integer> list_temp, int number) {
+        if (SIZE_BUFFER % 2 == 0) {
             boolean check = checkMedian_type(list_temp.get(4), list_temp.get(3));
             if (check) {
-//            System.out.println("List_creator: fifo(): " + solveMedianDouble(list_temp.get(4), list_temp.get(3)));
+                //            System.out.println("List_creator: fifo(): " + solveMedianDouble(list_temp.get(4), list_temp.get(3)));
                 System.out.println("[" + solveMedianDouble(list_temp.get(4), list_temp.get(3)) + "]");
             } else {
-//            System.out.println("List_creator: fifo(): " + solveMedianInt(list_temp.get(4), list_temp.get(3)));
+                //            System.out.println("List_creator: fifo(): " + solveMedianInt(list_temp.get(4), list_temp.get(3)));
                 System.out.println("[" + solveMedianInt(list_temp.get(4), list_temp.get(3)) + "]");
             }
+        } else {
+            System.out.println(returnMedianArray(list_temp));
+        }
+    }
+
+    //    main method
+    public  void fifo(LinkedList<Integer> list, int number) {
+        if (list.size() == getSizeBuffer()) {
+            LinkedList<Integer> list_temp = new LinkedList<>(list); // create temp arry
+            displayList(list);
+            printSortedPart(list_temp);
+            printMedianValue(list_temp, number);
             list.pop(); // remove first element
         }
         list.add(count, number);
 
-        if (count < 7) {
+        if (count < getIndexMax()) {
             count++;
         }
     }
-
 
 
 }
--- java_gb_2015/Java_Start_book_lesson/src/hours_80/dz2/Main.java
+++ java_gb_2015/Java_Start_book_lesson/src/hours_80/dz2/Main.java
@@ -5,11 +5,24 @@ import java.util.Random;
 
 import static hours_80.dz2.List_creator.*;
 
+/*
+Реализовать цикл, который будет 100 раз вызывать созданный вами метод fifo и будет передавать ему в аргументы
+целочисленное случайное число в интервале от 0 до 9 включительно. Метод fifo должен реализовать добавление элементов
+в буфер длиной 8 элементов. При достижении длины буфера заданного значения новые числа должны вытеснять самые старые
+числа. После каждого вызова метода fifo требуется распечатать в одной строке текущее содержимое буфера, отсортированное
+по возрастанию содержимое а также медианное значение.
+ */
 public class Main {
     public static void main(String[] args) {
         LinkedList<Integer> list = new LinkedList<>();
+        List_creator list_creator = new List_creator(8);
         for (int i = 0; i < 100; i++) {
-            fifo(list, randomNumber());
+            list_creator.fifo(list, randomNumber());
+        }
+
+        list_creator.setSizeBuffer(19);
+        for (int i = 0; i < 100; i++) {
+            list_creator.fifo(list, randomNumber());
         }
     }
 
